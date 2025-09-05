1d3860a57df9b26a242f874e327a4316caa3366c
kishima <kishima7@gmail.com>
2025-09-06 01:28:13 +0900

    add build.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # picocalc-r2p2
 PicoRuby shell for PicoCalc
+
+## build
+
+rake
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+#pico-sdk 2.2.0 requried
+rake picoruby:pico2:debug
+
+# Find the generated ELF file (exclude pico-sdk subdirectory)
+TARGET_ELF_FILE=$(find build/picoruby/pico2/debug -maxdepth 1 -name "R2P2-*.elf" -type f | head -1)
+
+if [[ -z "$TARGET_ELF_FILE" ]]; then
+    echo "Error: No ELF file found in build/picoruby/pico2/debug"
+    exit 1
+fi
+
+echo "Found ELF file: $TARGET_ELF_FILE"
+
+if [[ "$1" == "flash" ]]; then
+    openocd -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 2000" -c "program $TARGET_ELF_FILE verify reset exit"
+else
+    echo "Build successful. Use './build.sh flash' to write to device."
+fi
